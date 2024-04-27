64298f8632d11e3c6a2d571f45fdbf9360d76fe0
Md. Shahidullah <71697355+bandiota@users.noreply.github.com>
2024-04-27 20:02:16 +0600

    first commit

---
+++ myproject/settings.py
@@ -0,0 +1,153 @@
+from pathlib import Path
+import os
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = 'django-insecure-#3@wy3fkb^0o0$(d41+p+%y$=7k1xqd5r!&0a&b-=zsj!s&jev'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp',
+    'rest_framework',
+    'stockmgmgt',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    "whitenoise.runserver_nostatic",
+]
+
+CRISPY_TEMPLATE_PACK    =   'bootstrap4'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'myproject.urls'
+
+CSRF_COOKIE_SECURE = True 
+CSRF_COOKIE_HTTPONLY = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['template'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'myproject.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'myproject',
+#         'USER':'bandiota',
+#         'PASSWORD':'Nickson@255#',
+#         "HOST": "localhost",
+#         "PORT": "3306",
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': 'roundhouse.proxy.rlwy.net',
+#         'NAME': 'railway',
+#         'USER': 'root',
+#         'PORT': '17257',
+#         'PASSWORD': 'wmYZeeJpWbPMrOXjvniBadzAXDQcRSgP',
+#     }
+# }
+
+
+
+
+
+
+
+
+# User Model
+AUTH_USER_MODEL = 'myapp.User'
+
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Dhaka'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / "media/"
+
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
